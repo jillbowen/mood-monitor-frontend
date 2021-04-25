fd1ba2e7ea06434ab9a10c6db7b4644a52bd9d80
jillbowen <jillian.b.short@gmail.com>
2021-04-25 16:40:27 -0700

    passed days container to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchDays} from './actions/fetchDays';
+import DaysContainer from './containers/DaysContainer';
 
 class App extends React.Component {
   
   render () {
     return (
       <div className="App">
-        App
+        <DaysContainer />
       </div>
     )
   }
 }
 
-export default connect()(App);
+export default App;
