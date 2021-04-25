5a08153da7a2520c76dc82481c986ab4903588e5
jillbowen <jillian.b.short@gmail.com>
2021-04-25 15:04:30 -0700

    added fetchDays action

---
+++ src/actions/fetchDays.js
@@ -0,0 +1,7 @@
+export function fetchDays(action) {
+
+    return action
+    // fetch('http://localhost:3000/api/v1/days')
+    // .then(response => response.json())
+    // .then(response => console.log(response))
+}
