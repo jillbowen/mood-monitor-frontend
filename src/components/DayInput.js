dbcd8ca2ac0d35a3b7af206f6cd4341293cba6e1
jillbowen <jillian.b.short@gmail.com>
2021-04-25 16:41:47 -0700

    created day input component

---
+++ src/components/DayInput.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DayInput extends React.Component {
+    render() {
+        return (
+            <div>
+                Day Input
+            </div>
+        )
+    }
+}
+
+export default DayInput;
