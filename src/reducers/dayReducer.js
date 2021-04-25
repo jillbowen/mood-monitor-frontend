785a1be9768a6fb548521c9bcd015ab685d555b6
jillbowen <jillian.b.short@gmail.com>
2021-04-25 15:04:45 -0700

    updated to use dummy data for testing

--- src/reducers/dayReducer.js
+++ src/reducers/dayReducer.js
@@ -1,5 +1,5 @@
 // responsible for updating the parts of our store that have to do with day
 
 export default function dayReducer(state= {days: []}, action) {
-    return state
+    return action.payload
 }
