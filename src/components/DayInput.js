37ab923f9befc75ffa518dd10ebc827e3330b21c
jillbowen <jillian.b.short@gmail.com>
2021-04-25 23:27:28 -0700

    began building Day Input

--- src/components/DayInput.js
+++ src/components/DayInput.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 class DayInput extends React.Component {
+    
+    state = {
+        date: ''
+    }
+    
+    handleChange = (event) => {
+        this.setState({
+            [event.target.date]: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
-                Day Input
+                <form>
+                    <label>Date: </label>
+                    <input type="date" placeholder="Date of Mood Check-In" value={this.state.date} name="date" onChange={this.handleChange} />
+                </form>
             </div>
         )
     }
