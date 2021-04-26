df8690c9d8991eb3b09ae195d5b96303ca2bf137
jillbowen <jillian.b.short@gmail.com>
2021-04-25 17:51:51 -0700

    created fetchDays action

--- src/actions/fetchDays.js
+++ src/actions/fetchDays.js
@@ -1,6 +1,11 @@
 export function fetchDays(action) {
-    console.log('inside fetch days')
-    fetch('http://localhost:3000/api/v1/days')
-    .then(response => response.json())
-    .then(response => console.log(response))
+
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/days')
+        .then(response => response.json())
+        .then(response => dispatch({
+            type: 'FETCH_DAYS',
+            payload: response
+        }))
+    }
 }
--- src/reducers/dayReducer.js
+++ src/reducers/dayReducer.js
@@ -1,5 +1,11 @@
 // responsible for updating the parts of our store that have to do with day
 
 export default function dayReducer(state= {days: []}, action) {
-    
+    switch(action.type) {
+        case 'FETCH_DAYS':
+            return {days: action.payload}
+
+    default:
+        return state
+    }
 }
