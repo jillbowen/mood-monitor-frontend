137ab557faf7ce61677f1899c5a5d7aabe52358b
jillbowen <jillian.b.short@gmail.com>
2021-04-28 21:01:49 -0700

    added browser router

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import dayReducer from './reducers/dayReducer';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 import App from './App';
 
@@ -14,7 +15,9 @@ let store = createStore(dayReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>,
   document.getElementById('root')
 );
