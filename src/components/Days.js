51dc2b04c68bbc882e6e3abf934608e25efee463
jillbowen <jillian.b.short@gmail.com>
2021-05-01 14:23:19 -0700

    Added Link

--- src/components/Days.js
+++ src/components/Days.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Route, Link} from 'react-router-dom';
 import Day from './Day';
 
 const Days = (props) => {
     return (
         <div>
-            {props.days.map(day => <div key={day.id}><Day day={day} /></div>)} 
+            {props.days.map(day => <li key={day.id}><Link to={`/days/${day.id}`}>{day.date}</Link></li>)} 
         </div>
     )
 }
