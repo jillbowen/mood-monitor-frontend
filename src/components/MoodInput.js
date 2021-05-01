0e08a06963eca6a869af12b01d6639f298a4d028
jillbowen <jillian.b.short@gmail.com>
2021-05-01 14:45:15 -0700

    created mood input

--- src/components/MoodInput.js
+++ src/components/MoodInput.js
@@ -1,2 +1,14 @@
 import React from 'react';
+import MoodsContainer from '../containers/MoodsContainer';
 
+class MoodInput extends React.Component {
+    render() {
+        return(
+            <div>
+                Mood Input...
+            </div>
+        )
+    }
+}
+
+export default MoodInput;
