c37ed024f21369dbf5f7a64e1e6d02ede0761780
jillbowen <jillian.b.short@gmail.com>
2021-05-02 19:21:55 -0700

    changed slider to number inputs

--- src/components/MoodInput.js
+++ src/components/MoodInput.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { addMood } from '../actions/moods';
-import MoodSlider from './MoodSlider';
 
 class MoodInput extends React.Component {
     
@@ -12,7 +11,7 @@ class MoodInput extends React.Component {
         happy_rating: '',
         relaxed_rating: '',
         journal_entry: ''
-    }
+    } 
 
     handleChange = (event) => {
         this.setState({
@@ -22,29 +21,37 @@ class MoodInput extends React.Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
-        // addMood(this.state, this.props.id)
+        this.props.addMood(this.state, this.props.day.id)
+        this.setState({
+            awake_rating: '',
+            calm_rating: '',
+            confidence_rating: '',
+            happy_rating: '',
+            relaxed_rating: '',
+            journal_entry: ''
+        })
     }
     
     render() {
         return(
             <div>
                 <h2>How's Your Mood Today?</h2>
-                <p>(Scale: 1=low, 10=high)</p>
+                <h3>Rate Each Mood on a Scale of 1-10: 1=low, 10=high</h3>
                     <form onSubmit={this.handleSubmit}>
-                        <label>Rate Your Awakeness:</label><br/><br/><br/>
-                        <MoodSlider name="awake_rating" value={this.state.awake_rating} onChange={this.handleChange}/><br/>
+                        <label>Rate Your Awakeness:</label>
+                        <input type="number" min="1" max="10" value={this.state.awake_rating} name="awake_rating" onChange={this.handleChange}/><br/><br/>
+
+                        <label>Rate How Calm You Are:</label>
+                        <input type="number" min="1" max="10" value={this.state.calm_rating} name="calm_rating" onChange={this.handleChange}/><br/><br/>
                         
-                        <label>Rate How Clam You Are:</label><br/><br/><br/>
-                        <MoodSlider name="calm_rating" value={this.state.calm_rating} onChange={this.handleChange}/><br/>
+                        <label>Rate Your Confidence:</label>
+                        <input type="number" min="1" max="10" value={this.state.confidence_rating} name="confidence_rating" onChange={this.handleChange}/><br/><br/>
                         
-                        <label>Rate Your Confidence:</label><br/><br/><br/>
-                        <MoodSlider name="confidence_rating" value={this.state.confidence_rating} onChange={this.handleChange}/><br/>
+                        <label>Rate Your Happiness:</label>
+                        <input type="number" min="1" max="10" value={this.state.happy_rating} name="happy_rating" onChange={this.handleChange}/><br/><br/>
                         
-                        <label>Rate Your Happiness:</label><br/><br/><br/>
-                        <MoodSlider name="happy_rating" value={this.state.happy_rating} onChange={this.handleChange}/><br/>
-                        
-                        <label>Rate How Relaxed You Are:</label><br/><br/><br/>
-                        <MoodSlider name="relaxed_rating" value={this.state.relaxed_rating} onChange={this.handleChange}/><br/>
+                        <label>Rate How Relaxed You Are:</label>
+                        <input type="number" min="1" max="10" value={this.state.relaxed_rating} name="relaxed_rating" onChange={this.handleChange}/><br/><br/>
                         
                         <p>Quick Journal (optional):</p>
                         <textarea name="journal_entry" value={this.state.journal_entry} onChange={this.handleChange}></textarea><br/><br/>
@@ -56,4 +63,4 @@ class MoodInput extends React.Component {
     }
 }
 
-export default connect(null)(MoodInput);
+export default connect(null, {addMood})(MoodInput);
