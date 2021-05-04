9e52efcb4fdd598c4ac28cf5d8d064341d8cabf5
jillbowen <jillian.b.short@gmail.com>
2021-05-03 18:44:46 -0700

    finished building delete handler

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -1,26 +1,30 @@
 import React from 'react';
-
-const handleDelete = () => {
-    
-}
+import {connect} from 'react-redux';
+import {deleteMood} from '../actions/moods'
 
 const Moods = (props) => {
+    
+    const handleDelete = (mood) => {
+        props.deleteMood(mood.id, mood.day_id)
+    }
+    
     return (
         <div>
             {props.moods && props.moods.map(mood => 
                 <ul key={mood.id}> 
+                    <h4>Time of Check-In: {mood.created_at}</h4>
                     <li>Awake Rating: {mood.awake_rating}</li>
                     <li>Calm Rating: {mood.calm_rating}</li>
                     <li>Confidence Rating: {mood.confidence_rating}</li>
                     <li>Happy Rating: {mood.happy_rating}</li>
                     <li>Relaxed Rating: {mood.relaxed_rating}</li>
-                    <h3>Journal Entry:</h3>
+                    <h4>Journal Entry:</h4>
                     <p>{mood.journal_entry}</p>
-                    <button onClick={handleDelete}>Delete Check-In</button><br/><br/><br/>
+                    <button onClick={() => handleDelete(mood)}>Delete Check-In</button><br/><br/><br/>
                 </ul>
             )}
         </div>
     )
 }
 
-export default Moods;
+export default connect(null, {deleteMood})(Moods);
