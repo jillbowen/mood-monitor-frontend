74733e90115413400e9cdb963026d4367ce65ef8
jillbowen <jillian.b.short@gmail.com>
2021-05-03 22:26:29 -0700

    added home component

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+const Home = () => {
+        return(
+            <div className="homediv">
+                <h1 align="center">Welcome to Your Mood Monitor</h1><br/>
+                <h3 align="center">Use this tool to keep an eye on your daily mood, or your mood throughout the day.</h3>
+            </div>
+        )
+}
+
+export default Home;
