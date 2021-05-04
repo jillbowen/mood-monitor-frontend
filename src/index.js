0dd6df3c3603dc1cf93f15716d3d6b4b8e1a3286
jillbowen <jillian.b.short@gmail.com>
2021-05-03 22:26:22 -0700

    removed unused imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import dayReducer from './reducers/dayReducer';
 import {BrowserRouter as Router} from 'react-router-dom';
-
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
